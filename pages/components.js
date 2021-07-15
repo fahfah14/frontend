20662140be9a02cb2f68c59e5b5b344b43722c4d
fahfah14 <ketsirin161@gmail.com>
2021-07-15 13:42:49 +0700

    update 15/07/64

---
+++ pages/components.js
@@ -0,0 +1,359 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import React, { useState } from 'react';
+
+import { Button, Box, ButtonGroup,  } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Slider from '@material-ui/core/Slider';
+import Switch from '@material-ui/core/Switch';
+
+
+import TextField from '@material-ui/core/TextField';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import FolderIcon from '@material-ui/icons/Folder';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+import Breadcrumbs from '@material-ui/core/Breadcrumbs';
+
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  root1: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 200,
+  },
+  root2: {
+    width: 300,
+  },
+  root3: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+  root4: {
+    width: 500,
+  },
+  root5: {
+    width: '100%',
+  },
+  button: {
+    marginRight: theme.spacing(1),
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+}));
+
+function valuetext(value) {
+  return `${value}°C`;
+}
+
+function handleClick(event) {
+  event.preventDefault();
+  console.info('You clicked a breadcrumb.');
+}
+
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return 'Select campaign settings...';
+    case 1:
+      return 'What is an ad group anyways?';
+    case 2:
+      return 'This is the bit I really care about!';
+    default:
+      return 'Unknown step';
+  }
+}
+
+export default function Home() {
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const [value, setValue] = React.useState('recents');
+
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [skipped, setSkipped] = React.useState(new Set());
+  const steps = getSteps();
+
+  const isStepOptional = (step) => {
+    return step === 1;
+  };
+
+  const isStepSkipped = (step) => {
+    return skipped.has(step);
+  };
+
+  const handleNext = () => {
+    let newSkipped = skipped;
+    if (isStepSkipped(activeStep)) {
+      newSkipped = new Set(newSkipped.values());
+      newSkipped.delete(activeStep);
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped(newSkipped);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleSkip = () => {
+    if (!isStepOptional(activeStep)) {
+      // You probably want to guard against something like this,
+      // it should never occur unless someone's actively trying to break something.
+      throw new Error("You can't skip a step that isn't optional.");
+    }
+
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    setSkipped((prevSkipped) => {
+      const newSkipped = new Set(prevSkipped.values());
+      newSkipped.add(activeStep);
+      return newSkipped;
+    });
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div>
+      <Head>
+       <title>รับออกแบบและพัฒนาเว็บไซต์</title>
+      </Head>
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link href="/"><a>Home</a></Link> |  
+            <Link href="/about"><a>About</a></Link> |    
+            <Link href="/service"><a>Service</a></Link> |    
+            <Link href="/products/P001"><a>Products</a></Link> |
+            <Link href="/components"><a>Components</a></Link>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      </div>
+      <div>
+        <h1><center>Index Page</center></h1>
+        <br/>
+        <center>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <br /><br /><br />
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+        <br /><br /><br />
+        <div className={classes.root}>
+      <Button variant="contained">Default</Button>
+      <Button variant="contained" color="primary">
+        Primary
+      </Button>
+      <Button variant="contained" color="secondary">
+        Secondary
+      </Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" color="primary" href="#contained-buttons">
+        Link
+      </Button>
+      </div>
+      <br /><br /><br />
+      <form className={classes.container} noValidate>
+      <TextField
+        id="date"
+        label="Birthday"
+        type="date"
+        defaultValue="2017-05-24"
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      </form>
+      <br /><br /><br />
+      <div className={classes.root}>
+      <Typography id="discrete-slider-small-steps" gutterBottom>
+        Small steps
+      </Typography>
+      <Slider
+        defaultValue={0.00000005}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider-small-steps"
+        step={0.00000001}
+        marks
+        min={-0.00000005}
+        max={0.0000001}
+        valueLabelDisplay="auto"
+      />
+    </div>
+    <br /><br /><br />
+    <div>
+      <Switch
+        checked={state.checkedA}
+        onChange={handleChange}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <Switch
+        checked={state.checkedB}
+        onChange={handleChange}
+        color="primary"
+        name="checkedB"
+        inputProps={{ 'aria-label': 'primary checkbox' }}
+      />
+      <Switch inputProps={{ 'aria-label': 'primary checkbox' }} />
+      <Switch disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
+      <Switch disabled checked inputProps={{ 'aria-label': 'primary checkbox' }} />
+      <Switch
+        defaultChecked
+        color="default"
+        inputProps={{ 'aria-label': 'checkbox with default color' }}
+      />
+    </div>
+    <br /><br /><br />
+    <form className={classes.root3} noValidate autoComplete="off">
+      <TextField id="standard-basic" label="Standard" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </form>
+    <br /><br /><br />
+    <BottomNavigation value={value} onChange={handleChange} className={classes.root4}>
+      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+    </BottomNavigation>
+    <br /><br /><br />
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link color="inherit" href="/" onClick={handleClick}>
+        Material-UI
+      </Link>
+      <Link color="inherit" href="/getting-started/installation/" onClick={handleClick}>
+        Core
+      </Link>
+      <Typography color="textPrimary">Breadcrumb</Typography>
+    </Breadcrumbs>
+    <br /><br /><br />
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label, index) => {
+          const stepProps = {};
+          const labelProps = {};
+          if (isStepOptional(index)) {
+            labelProps.optional = <Typography variant="caption">Optional</Typography>;
+          }
+          if (isStepSkipped(index)) {
+            stepProps.completed = false;
+          }
+          return (
+            <Step key={label} {...stepProps}>
+              <StepLabel {...labelProps}>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
+      <div>
+        {activeStep === steps.length ? (
+          <div>
+            <Typography className={classes.instructions}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Button onClick={handleReset} className={classes.button}>
+              Reset
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+            <div>
+              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+                Back
+              </Button>
+              {isStepOptional(activeStep) && (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleSkip}
+                  className={classes.button}
+                >
+                  Skip
+                </Button>
+              )}
+
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleNext}
+                className={classes.button}
+              >
+                {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+        </center>
+    
+    </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,69 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import { Button, Box, ButtonGroup,  } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
-  <div>
-    <Head>
-      <title>รับออกแบบพัฒนาเว็บไซต์</title>
-    </Head>
-    <main> 
-      <h1><center>Index Page</center></h1>
-      <br />
-      <center>
-      <Link href="/"><a>Home</a></Link> |
-      <Link href="/about"><a>Abou</a></Link> |
-      <Link href="/service"><a>Service</a></Link> |
-      <Link href="/products/P001"><a>Products</a></Link>
-      </center>
-    </main>
-  </div>
+    <div>
+      <Head>
+       <title>รับออกแบบและพัฒนาเว็บไซต์</title>
+      </Head>
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <Link href="/"><a>Home</a></Link> |  
+            <Link href="/about"><a>About</a></Link> |    
+            <Link href="/service"><a>Service</a></Link> |    
+            <Link href="/products/P001"><a>Products</a></Link> |
+            <Link href="/components"><a>Components</a></Link>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+      <div>
+        <h1><center>Index Page</center></h1>
+        <br/>
+        <center>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <br /><br /><br />
+        <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup>
+        <br /><br /><br />
+        </center>
+      </div>
+    </div>
   )
 }
